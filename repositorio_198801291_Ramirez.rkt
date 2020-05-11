534413cb716a36d395ef015ced60c5c0d0beef39
David Ramirez <david.ramirez.v@usach.cl>
2020-05-11 18:45:59 -0400

    Eliminado archivo de prueba, Comenzado comando git

--- repositorio_198801291_Ramirez.rkt
+++ repositorio_198801291_Ramirez.rkt
@@ -1,21 +1,42 @@
 #lang racket
 
-; en vista deque debo probar el codigo creare  un archivo de prueba
-;a la izquierda el commit, a la derecha el codigo del commit
-;en el futuro de implementar que el usuario cambie el string del commit
-;y cambiar el codigo de este commit
-(define (comentario)
-  (cons "commit: cambie una linea del codigo" "codigo: 2146314")
-)
+;---TDA Zonas-------
 
+
+
+
+
+(define codigo 0)
 
 ;-----------comandos-----------
 
 ;commit
-(define commit(lambda (comentario codigo)
-         (displayln "Has realizado un commit:")
-         (displayln comentario)
-         (displayln "Codigo unico del commit:")
-         (displayln codigo)               
-      )  
-  )
+(define commit(lambda (comentario)
+    (display "Has realizado un commit: ")
+    (displayln comentario)
+    (display "Codigo unico del commit: ")
+    (displayln codigo)
+    (set! codigo (+ codigo 1))
+    )  
+)
+
+
+;-------------git---------
+
+;pull -> add -> commit -> push
+
+(define (git comando)
+    (cond
+      ;pull
+      
+      ;add
+      
+      ;commit
+      ;(git (commit "soy un comentario"))
+      [(eq? comando commit)
+       (commit)]
+      
+      ;push
+      
+    )
+)
