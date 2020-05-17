bd7da89eb9d97f742e7ad6fd39129ec1bd83ff5d
David Ramirez <david.ramirez.v@usach.cl>
2020-05-17 17:35:00 -0400

    idea sobre la zonas TDA

--- repositorio_198801291_Ramirez.rkt
+++ repositorio_198801291_Ramirez.rkt
@@ -1,25 +1,31 @@
 #lang racket
 
-;---TDA Zonas-------
-
-
-
-
-
-(define codigo 0)
-
-;-----------comandos-----------
-
-;commit
-(define commit(lambda (comentario)
-    (display "Has realizado un commit: ")
-    (displayln comentario)
-    (display "Codigo unico del commit: ")
-    (displayln codigo)
-    (set! codigo (+ codigo 1))
-    )  
+;---TDA------
+;TDA zonas
+(define (zonas)
+  (define workplace (list))
+  (define index (list))
+  (define loca-repository (list))
+  (define remote-repository (list))
+  (list workplace index loca-repository remote-repository)
 )
 
+;TDA archivos
+(define (archivo)
+ )
+
+
+
+;-----------comandos-----------
+;pull
+
+;add
+
+
+;commit
+
+
+;push
 
 ;-------------git---------
 
