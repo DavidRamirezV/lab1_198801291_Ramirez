0ab9316c3a16a42108c3717b5cd78d4d0c3ce3de
David Ramirez <david.ramirez.v@usach.cl>
2020-05-17 22:34:03 -0400

    Comenzadas funciones commit, add, pull y push

--- repositorio_198801291_Ramirez.rkt
+++ repositorio_198801291_Ramirez.rkt
@@ -1,31 +1,58 @@
 #lang racket
 
 ;---TDA------
-;TDA zonas
+
+;Workspace
+
+;Index
+
+;Local Repository
+
+;Remote Repository
+
+;Archivo
+
+;Zonas TDA
 (define (zonas)
-  (define workplace (list))
-  (define index (list))
-  (define loca-repository (list))
-  (define remote-repository (list))
-  (list workplace index loca-repository remote-repository)
+  (define workspace null)
+  (define index null)
+  (define local-repository null)
+  (define remote-repository null)
+  (list workspace index local-repository remote-repository)
 )
-
-;TDA archivos
-(define (archivo)
- )
-
 
 
 ;-----------comandos-----------
 ;pull
-
+(define (pull)
+  (lambda (commits)
+        (displayln "Commits recibidos desde remote-repository")
+    )
+)
+  
 ;add
 
-
+(define (add)
+  (lambda (archivo1)
+        (displayln "Cambios desde Workspace a Index")
+    )
+)
+  
 ;commit
-
-
+(define (commit)
+  (lambda (comentario)
+        (displayln "Cambio realizado: ")
+        (displayln comentario)
+    )
+)
+  
 ;push
+(define (push)
+  (lambda (commits)
+        (displayln "Commits agregado a remote-repository")
+    )
+)
+  
 
 ;-------------git---------
 
@@ -34,15 +61,32 @@
 (define (git comando)
     (cond
       ;pull
-      
+      [(eq? comando pull)
+       (pull)]
       ;add
-      
+      [(eq? comando add)
+       (add)]
+
       ;commit
-      ;(git (commit "soy un comentario"))
+      ;((git commit) "soy un comentario")
       [(eq? comando commit)
        (commit)]
       
       ;push
-      
+      [(eq? comando push)
+       (push)]
     )
+)
+
+;Unir Listas
+(define unir-listas
+  (lambda (l1 l2)
+      (if (null? l1)
+          l2
+          (if (null? l2)
+              l1
+              (cons (car l1) (unir-listas (cdr l1) l2))                    
+          )
+       )
+  )
 )
